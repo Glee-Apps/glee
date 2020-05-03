c6dba66054b360fcc1451137a783fa02db1a4300
Manuhmutua <manuh.mutuah@gmail.com>
2020-05-03 19:19:26 +0300

    Added base code

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/Manuhmutua/glee
+
+go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/jinzhu/gorm v1.9.12
+	github.com/joho/godotenv v1.3.0
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Manuhmutua/glee/app"
+	"github.com/Manuhmutua/glee/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
+
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	//router.NotFoundHandler = app.NotFoundHandler
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}

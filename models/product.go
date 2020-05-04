befa695e8ce9a68c72aacc38528c84e030a2b128
Manuhmutua <manuh.mutuah@gmail.com>
2020-05-04 10:09:10 +0300

    Refactored contacts to products

--- models/product.go
+++ models/product.go
@@ -72,22 +72,22 @@ func (product *Product) Create() map[string]interface{} {
 
 func GetProduct(id uint) *Product {
 
-	contact := &Product{}
-	err := GetDB().Table("contacts").Where("id = ?", id).First(contact).Error
+	product := &Product{}
+	err := GetDB().Table("products").Where("id = ?", id).First(product).Error
 	if err != nil {
 		return nil
 	}
-	return contact
+	return product
 }
 
 func GetProducts(user uint) []*Product {
 
-	contacts := make([]*Product, 0)
-	err := GetDB().Table("contacts").Where("user_id = ?", user).Find(&contacts).Error
+	products := make([]*Product, 0)
+	err := GetDB().Table("products").Where("user_id = ?", user).Find(&products).Error
 	if err != nil {
 		fmt.Println(err)
 		return nil
 	}
 
-	return contacts
+	return products
 }
